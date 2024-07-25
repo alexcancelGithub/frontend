95f5419d981a8d791ebceead6c3411f005d06910
AlexcCancelHTSR <alexandru.cancel@htsg.eu>
2024-07-25 15:58:29 +0300

    starting from the bottom

--- src/routes/foodieRoutes.tsx
+++ src/routes/foodieRoutes.tsx
@@ -1,14 +1,28 @@
 import { Route, Routes } from "react-router-dom";
 import { FE_ROUTES } from "../constants";
+import { Suspense, lazy } from "react";
 
 const { BASE, LOGIN, SIGNUP } = FE_ROUTES;
+
+// TODO: to make a Loading Spinner
+const Loadable =
+  (Component: React.ComponentType<any>) => (props: JSX.IntrinsicAttributes) =>
+    (
+      <Suspense fallback={<p>Loading...</p>}>
+        <Component {...props} />
+      </Suspense>
+    );
+
+const HomePage = Loadable(lazy(() => import("../pages/Home")));
+const LogInPage = Loadable(lazy(() => import("../pages/LogIn")));
+const SignUpPage = Loadable(lazy(() => import("../pages/SignUp")));
 
 export const FoodieRoutes = () => {
   return (
     <Routes>
-      <Route path={BASE} element={<h1>HOME PAGE</h1>} />
-      <Route path={LOGIN} element={<h1>LOGIN PAGE</h1>} />
-      <Route path={SIGNUP} element={<h1>SIGNUP PAGE</h1>} />
+      <Route path={BASE} element={<HomePage />} />
+      <Route path={LOGIN} element={<LogInPage />} />
+      <Route path={SIGNUP} element={<SignUpPage />} />
     </Routes>
   );
 };
